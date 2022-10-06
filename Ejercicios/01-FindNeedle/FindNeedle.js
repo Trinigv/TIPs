b28572ba2b67e59b7a423f8944647a989fea4e80
Trinigv <99trini@gmail.com>
2022-10-06 20:02:46 -0300

    First Excercise

--- Ejercicios/01-FindNeedle/FindNeedle.js
+++ Ejercicios/01-FindNeedle/FindNeedle.js
@@ -1,6 +1,22 @@
-function FindNeedle (haystack, needle) {
-  // Your code here:
-
+function FindNeedle(haystack, needle) {
+	// Your code here:
+	const finding = haystack.search(needle);
+	return finding;
 }
 
-module.exports = FindNeedle
+function FindNeedleTwo(haystack, needle) {
+	for (let i = 0; i < haystack.length; i++) {
+		if (needle === haystack.slice(i, i + needle.length)) {
+			console.log(haystack.slice(i, i + needle.length));
+			return i; // si coincide se corta ejecucion y retorna el indice
+		}
+	}
+	return -1; //sino retorna -1
+}
+
+//complejidad temporal -----> O(n * m) siendo n el length del haystack y m el length del needle
+// la complejidad temporal es la cantidad de comparaciones que debe realizar el algoritmo.
+
+console.log(FindNeedleTwo('react-redux', 'redux'));
+
+module.exports = FindNeedle;
