190157b9baeb1dfa8f46fec066db3da7331cf1fc
Trinigv <99trini@gmail.com>
2022-10-07 23:56:39 -0300

    MaxValue

--- Ejercicios/03-MaxValue/MaxValue.js
+++ Ejercicios/03-MaxValue/MaxValue.js
@@ -1,6 +1,23 @@
-function MaxValue (shares) {
-  // Your code here:
-
+function MaxValue(shares) {
+	// Your code here:
+	// tengo que recorrer el array
+	// el mayor profit es el valor max - valor min
+	// busco el max y min comparandolos con un pointer anterior
+	// resto y obtengo profit
+	var sell = shares[0];
+	var buy = shares[0];
+	for (let i = 0; i < shares.length; i++) {
+		if (sell < shares[i]) {
+			sell = shares[i];
+		} else if (shares[i] < buy) {
+			buy = shares[i];
+		}
+	}
+	console.log(sell, buy);
+	var profit = sell - buy;
+	return profit;
 }
 
-module.exports = MaxValue
+console.log(MaxValue([11, 5, 6, 7, 13, 3, 4]));
+
+module.exports = MaxValue;
